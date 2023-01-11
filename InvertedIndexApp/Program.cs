70d903114c01be1a0f1ce19df9f8f595da90d6a3
ShawTheatre <ShawTheatre1@gmail.com>
2023-01-12 00:32:45 +0200

    Add swagger

--- InvertedIndexApp/Program.cs
+++ InvertedIndexApp/Program.cs
@@ -3,18 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IndexService>();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/is-indexed", (IndexService indexService) => indexService.IsIndexed);
 
-app.MapPost("/index", ([FromQuery] int threadsCount, IndexService indexService) => new
+app.MapPost("/index", ([FromQuery] int? threadsCount, IndexService indexService) => new
 {
-    duration = indexService.Index()
+    duration = indexService.Index(threadsCount ?? 1)
 });
 
 app.MapGet("/query", ([FromQuery] string word, IndexService indexService) => {
+    if (!indexService.IsIndexed) return Results.BadRequest();
     var result = indexService.Query(word);
     if (result is null) return Results.NotFound();
     return Results.Ok(result);
